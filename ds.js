c13b3aabf3fdd72addbc82e15fbf4ec0139287ff
Brett Nelson <brettcnelson@gmail.com>
2017-02-04 14:16:00 -0600

    adds iframe setup

--- ds.js
+++ ds.js
@@ -1,1 +1,13 @@
-'http://forecast.io/embed/#lat=55.6164&lon=12.0555&name=test'
+document.getElementById('loc').onclick = function () {
+	getLocation(true)
+}
+
+var iframe = document.getElementById('forecast_embed')
+
+
+
+function getLocation(force) {
+	console.log(force)
+}
+
+// 'http://forecast.io/embed/#lat=55.6164&lon=12.0555&name=test'
