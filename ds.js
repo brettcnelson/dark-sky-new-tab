f64943e3e7fc67c98587666c729cd9d329db726e
Brett Nelson <brettcnelson@gmail.com>
2017-02-04 16:37:06 -0600

    expands html skeleton

--- ds.js
+++ ds.js
@@ -2,12 +2,36 @@ document.getElementById('loc').onclick = function () {
 	getLocation(true)
 }
 
+function getLocation(force) {
+	console.log(force)
+}
+
+document.getElementById('saveLoc').addEventListener('click', saveLoc)
+
+function saveLoc() {
+	console.log('saved')
+}
+
+document.getElementById('clearLS').addEventListener('click', clearLS)
+
+function clearLS() {
+	console.log('clearLSed')
+}
+
+
+document.getElementById('clearSS').addEventListener('click', clearSS)
+
+function clearSS() {
+	console.log('clearSSed')
+}
+
+
+
+
 var iframe = document.getElementById('forecast_embed')
 
 
 
-function getLocation(force) {
-	console.log(force)
-}
+
 
 // 'http://forecast.io/embed/#lat=55.6164&lon=12.0555&name=test'
