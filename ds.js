75a69351915604f8c98d81d6b634c1b5febf2b0e
Brett Nelson <brettcnelson@gmail.com>
2017-02-04 21:36:20 -0600

    adds css

--- ds.js
+++ ds.js
@@ -2,36 +2,46 @@ document.getElementById('loc').onclick = function () {
 	getLocation(true)
 }
 
+document.getElementById('saveLoc').onclick = function () {
+	console.log('saved')
+}
+
+document.getElementById('clearLS').onclick = function () {
+	console.log('lsbefore', localStorage)
+	localStorage.clear()
+	console.log('lsafter', localStorage)
+}
+
+document.getElementById('clearSS').onclick = function () {
+	console.log('ssbefore', sessionStorage)
+	sessionStorage.clear()
+	console.log('ssafter', sessionStorage)
+}
+
+
 function getLocation(force) {
 	console.log(force)
 }
 
-document.getElementById('saveLoc').addEventListener('click', saveLoc)
-
-function saveLoc() {
-	console.log('saved')
-}
-
-document.getElementById('clearLS').addEventListener('click', clearLS)
-
-function clearLS() {
-	console.log('clearLSed')
-}
-
-
-document.getElementById('clearSS').addEventListener('click', clearSS)
-
-function clearSS() {
-	console.log('clearSSed')
-}
-
-
-
 
 var iframe = document.getElementById('forecast_embed')
 
-
-
-
-
 iframe.src = 'http://forecast.io/embed/#lat=42.667869&lon=-70.840479&name=test home'
+
+function initAutocomplete() {
+	var input = document.getElementById('locSearch');
+	autocomplete = new google.maps.places.Autocomplete(input)
+	autocomplete.addListener('place_changed', changeSrc)
+}
+
+function changeSrc() {
+	var locName = document.getElementById('locSearch').value
+	locLat = autocomplete.getPlace().geometry.location.lat().toFixed(4)
+	locLon = autocomplete.getPlace().geometry.location.lng().toFixed(4)
+	
+	sessionStorage.pending = "http://forecast.io/embed/#lat=" + locLat + "&lon=" + locLon + "&name=" + locName
+
+	sessionStorage.setItem(locName, iframe.src)
+
+	location.reload()
+}
