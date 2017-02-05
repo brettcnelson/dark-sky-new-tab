d53d26d8c10848dfe8f45198d6060957ab58dced
Brett Nelson <brettcnelson@gmail.com>
2017-02-05 01:13:31 -0600

    fixes api csp problem

--- ds.js
+++ ds.js
@@ -6,42 +6,30 @@ document.getElementById('saveLoc').onclick = function () {
 	console.log('saved')
 }
 
-document.getElementById('clearLS').onclick = function () {
+document.getElementById('remove').onclick = function () {
 	console.log('lsbefore', localStorage)
 	localStorage.clear()
 	console.log('lsafter', localStorage)
 }
 
-document.getElementById('clearSS').onclick = function () {
+document.getElementById('clear').onclick = function () {
 	console.log('ssbefore', sessionStorage)
 	sessionStorage.clear()
 	console.log('ssafter', sessionStorage)
 }
 
+var iframe = document.getElementById('forecast_embed')
+
+document.getElementById('loading').style.display = 'none'
+
+document.getElementById('locSearch').focus()
+
+
+
+
+
+
 
 function getLocation(force) {
 	console.log(force)
-}
-
-
-var iframe = document.getElementById('forecast_embed')
-
-iframe.src = 'http://forecast.io/embed/#lat=42.667869&lon=-70.840479&name=test home'
-
-function initAutocomplete() {
-	var input = document.getElementById('locSearch');
-	autocomplete = new google.maps.places.Autocomplete(input)
-	autocomplete.addListener('place_changed', changeSrc)
-}
-
-function changeSrc() {
-	var locName = document.getElementById('locSearch').value
-	locLat = autocomplete.getPlace().geometry.location.lat().toFixed(4)
-	locLon = autocomplete.getPlace().geometry.location.lng().toFixed(4)
-	
-	sessionStorage.pending = "http://forecast.io/embed/#lat=" + locLat + "&lon=" + locLon + "&name=" + locName
-
-	sessionStorage.setItem(locName, iframe.src)
-
-	location.reload()
 }
