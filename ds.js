8bf5c4be2e9f289fc9c76e4508815d795da6569c
Brett Nelson <brettcnelson@gmail.com>
2017-02-12 09:45:34 -0600

    solves ifram src reload

--- ds.js
+++ ds.js
@@ -138,25 +138,15 @@ function popList() {
 			}
 		}
 		el.onclick = function() {
+			if (localStorage[this.innerHTML]) {
+				var newsrc = localStorage[this.innerHTML]
+			}
+			else {
+				var newsrc = sessionStorage[this.innerHTML]
+			}
+			sessionStorage.pending = newsrc
 			search.value = this.innerHTML
-			search.focus()
-			var keyboardEvent = document.createEvent("KeyboardEvent");
-var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
-
-
-keyboardEvent[initMethod](
-                   "keydown", // event type : keydown, keyup, keypress
-                    true, // bubbles
-                    true, // cancelable
-                    window, // viewArg: should be window
-                    false, // ctrlKeyArg
-                    false, // altKeyArg
-                    false, // shiftKeyArg
-                    false, // metaKeyArg
-                    40, // keyCodeArg : unsigned long the virtual key code, else 0
-                    0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
-);
-document.dispatchEvent(keyboardEvent);
+			location.reload()
 		}
 	}
 }
