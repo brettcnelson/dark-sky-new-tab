fcf4672ea002e61cf55efb77ebd0f99e386d133b
Brett Nelson <brettcnelson@gmail.com>
2019-03-15 05:40:41 -0400

    edits syncing

--- ds.js
+++ ds.js
@@ -1,4 +1,8 @@
-google.maps.event.addDomListener(window, 'load', initAutocomplete);
+google.maps.event.addDomListener(window, 'load', () => {
+	var input = document.getElementById('locSearch');
+	autocomplete = new google.maps.places.Autocomplete(input)
+	autocomplete.addListener('place_changed', changeSrc)
+});
 var tables = document.getElementById('tables');
 var prevSaved = document.getElementById('prevsaved');
 var iframe = document.getElementById('forecast_embed');
@@ -8,53 +12,62 @@ var defaults = {backgroundColor: '#9C9C9C',font: 'Helvetica',units: '',color: ''
 document.getElementById('colordefault').style.backgroundColor = defaults.backgroundColor;
 var options;
 var data = {};
-chrome.storage.sync.get(defaults, setOptions);
-document.getElementById('locSearch').focus();
-
-// ************** SET DEFAULT NOTES ****************************
-// document.getElementById('default').onclick = function() {
-// 	// console.log(iframe.src, sessionStorage);
-// 	for (var key in sessionStorage) {
-// 		if (sessionStorage[key] === iframe.src) {
-// 			var defaults = [iframe.src, sessionStorage['*' + key]];
-// 		}
-// 	}
-// 	localStorage.setItem('*default', JSON.stringify(defaults));
-// 	console.log(localStorage['*default']);
-// }
-
-// if (localStorage['*default']) {
-// 	console.log('def', localStorage['*default'][0]);
-// 	iframe.src = JSON.parse(localStorage['*default'])[0];
-// }
-
-// change this to else if - if the above if statement is uncommented
-//*******************************************************************
-
-updatePlaceCount();
-if (iframe.src.split('forecast.io/embed/').length) {
-	if (sessionStorage['*results']) {
-		data.name = JSON.parse(sessionStorage['*results']).name;
+var state = {deleteSaved:false};
+chrome.storage.sync.get(defaults, (opts) => {
+	setOptions(opts);
+	document.getElementById('locSearch').focus();
+	if (!sessionStorage['*pending']) {
+		getLocation();
 	}
 	else {
-		data.name = iframe.src.split('&name=')[1].split('&color')[0].replace(/%20/gi,' ');
+		updatePlaceCount();
+		if (sessionStorage['*results']) {
+			data.name = JSON.parse(sessionStorage['*results']).name;
+		}
+		if (sessionStorage['*pending']) {
+			if (!data.name) {
+				data.name = sessionStorage['*pending'].split('&name=')[1].replace(/%20/gi,' ');
+			}
+			if (sessionStorage['*geocode']) {
+				locBtn.style.display = 'none';
+				sessionStorage.removeItem('*geocode');
+			}
+			console.log(options)
+			iframe.src = `${sessionStorage['*pending']}&color=${options.color}&font=${options.font}&units=${options.units}`;
+			sessionStorage.removeItem('*pending');
+			geosrc(data.name);
+			popList();
+		}
 	}
-}
-if (sessionStorage['*pending']) {
-	if (sessionStorage['*geocode']) {
-		locBtn.style.display = 'none';
-		sessionStorage.removeItem('*geocode');
-	}
-	iframe.src = sessionStorage['*pending'];
-	sessionStorage.removeItem('*pending');
-	geosrc();
-	popList();
-}
-else {
-	getLocation();
-}
+});
 
-// FUNCTIONS ***********************************************************
+
+// no iframe, pending AND results, pending AND no results
+
+
+
+// FUNCTIONS ***************************
+
+function changeSrc() {
+	var results = autocomplete.getPlace();
+	if (results.photos) {
+		var photourls = [];
+		for (var i = 0 ; i < results.photos.length ; i++) {
+			photourls.push(results.photos[i].getUrl({'maxWidth': 500, 'maxHeight': 200}));
+		}
+		results.photourls = photourls;
+	}
+	var locName = results.formatted_address;
+	var frameName = results.name;
+	locLat = results.geometry.location.lat().toFixed(4);
+	locLon = results.geometry.location.lng().toFixed(4);
+	var url = "https://forecast.io/embed/#lat=" + locLat + "&lon=" + locLon + "&name=" + frameName;
+	sessionStorage['*pending'] = url;
+	sessionStorage.setItem(locName, url);
+	sessionStorage.setItem('*' + locName, JSON.stringify(results));
+	sessionStorage.setItem('*results', JSON.stringify(results));
+	location.reload();
+}
 
 function setOptions(opts) {
 	var {backgroundColor,...rest} = opts;
@@ -63,28 +76,52 @@ function setOptions(opts) {
 	options = rest;
 }
 
-function updatePlaceCount() {
-	showTables.innerHTML = '\u2606: ' + getPlaces()[0];
+function getLocation(force) {
+	iframe.style.display = 'none';
+	document.getElementById('framecon').style.display = '';
+	sessionStorage.removeItem('*results');
+	document.getElementById('loc').style.display = 'none';
+	navigator.geolocation.getCurrentPosition(function(p) {
+		var locLat = p.coords.latitude;
+		var locLon = p.coords.longitude;
+		var geocoder = new google.maps.Geocoder;
+		geocoder.geocode({'location': {lat: locLat, lng: locLon}}, function(results, status) {
+			if (status === 'OK') {
+				var address = results[0].address_components;
+				data.name = address[0].short_name + ' ' + address[1].short_name;
+				var src = "https://forecast.io/embed/#lat=" + locLat.toFixed(4) + "&lon=" + locLon.toFixed(4) + "&name=" + data.name;
+				sessionStorage.setItem(data.name, src);
+				if (force) {
+					sessionStorage['*pending'] = src;
+					sessionStorage['*geocode'] = true;
+					location.reload();
+				}
+				iframe.src = `${src}&color=${options.color}&font=${options.font}&units=${options.units}`;
+				geosrc(data.name);
+				showLinks();
+				popList();
+				updatePlaceCount();
+			}
+		})
+	}, function(error) {
+			document.getElementById('top-container').style.display = ''
+			popList()
+			console.log('err =', error)
+			var err = document.getElementById('error')
+			if (error.code === 1) {
+				err.innerHTML = 'It loooks like you haven\'t enabled location permission.  Search for a location below'
+			}
+			if (error.code === 2) {
+				err.innerHTML = 'For some reason your location is unavailable - is your wifi on? modem/router working?<br>Sometimes Chrome\'s location service goes down for a little while.<br>Just search for a location below<br>\u2193'
+			}
+			if (error.code === 3) {
+				err.innerHTML = 'The location search timed out.  Search for a location below'
+			}
+			document.getElementById('saveLocbtn').style.display = 'none'	
+	});
 }
 
-function getPlaces() {
-	var places = 0;
-	var recent = 0;
-	for (var key in localStorage) {
-		if (key[0] !== '*' && key !== 'length' && typeof localStorage[key] !== 'function') {
-			places++;
-		}
-	}
-	for (var key in sessionStorage) {
-		if (key[0] !== '*' && key !== 'length' && typeof sessionStorage[key] !== 'function') {
-			recent++;
-		}
-	}
-	return [places, recent];
-}
-
-function geosrc() {
-	var name = getName();
+function geosrc(name) {
 	document.getElementById('dsanch').innerHTML = name + ' at Dark Sky';
 	var lat = iframe.src.split('=')[1].split('&')[0];
 	var lng = iframe.src.split('&lon=')[1].split('&')[0];
@@ -101,21 +138,32 @@ function geosrc() {
 		}, 2000);
 	}
 	else {
-		var text = name + ' on Google Maps';
-		var url = 'http://maps.google.com/?q=' + lat + ',' + lng;
-		mapAnch.innerHTML = text;
-		mapAnch.href = url;
+		mapAnch.innerHTML = name + ' on Google Maps';
+		mapAnch.href = 'http://maps.google.com/?q=' + lat + ',' + lng;
 	}
 }
 
-function getName() {
-	return name;
+function showLinks() {
+// 	var links = document.getElementsByClassName('links')
+	iframe.style.display = ''
+	document.getElementById('framecon').style.display = 'none'
+// 	links[0].style.display = ''
+// 	links[1].style.display = ''
+
+// 	//hl
+// 		// var links = document.getElementsByClassName('links');
+// 	// iframe.style.display = 'none';
+// 	// document.getElementById('framecon').style.display = '';
+// 	// links[0].style.display = 'none';
+// 	// links[1].style.display = 'none';
 }
 
 function popList() {
 	var currInLocal = false;
 	for (var key in localStorage) {
-		if (!currInLocal && (key === iframe.src.split('&name=')[1].replace(/%20/gi,' ') || (sessionStorage['*results'] && key === JSON.parse(sessionStorage['*results']).formatted_address))) {
+		if (!currInLocal && key === data.name ) {
+			// iframe.src.split('&name=')[1].replace(/%20/gi,' ')
+			// || (sessionStorage['*results'] && key === JSON.parse(sessionStorage['*results']).formatted_address)
 			currInLocal = true;
 			document.getElementById('saveLocbtn').style.display = 'none'
 			document.getElementById('prevsaved').style.display = '';		
@@ -126,7 +174,7 @@ function popList() {
 		document.getElementById('prevsaved').style.display = 'none';	
 	}
 
-	function pop(id1, id2, storage) {
+	function pop(id1, storage) {
 		var table = document.getElementById(id1);
 		while (table.rows.length) {
 			table.deleteRow(0);
@@ -142,8 +190,8 @@ function popList() {
 		}
 	}
 
-	pop('recent', 'rectable', sessionStorage);
-	pop('saved', 'savtable', localStorage);
+	pop('recent', sessionStorage);
+	pop('saved', localStorage);
 
 	var places = document.querySelectorAll('.places');
 	for (var i = 0 ; i < places.length ; i++) {
@@ -151,8 +199,8 @@ function popList() {
 		var search = document.getElementById('locSearch');
 
 		el.onclick = function() {
-			if (deleteSaved) {
-				deleteSaved = false;
+			if (state.deleteSaved) {
+				state.deleteSaved = false;
 				var table = document.getElementById('savtable');
 				table.deleteRow(1);
 				document.getElementById('deleteSaved').innerHTML = 'remove one';
@@ -183,9 +231,6 @@ function popList() {
 						sessionStorage.removeItem('*results');
 					}
 				}
-				if (!newsrc) {
-					newsrc = iframe.src;
-				}
 				sessionStorage['*pending'] = newsrc;
 				location.reload();
 			}
@@ -193,91 +238,24 @@ function popList() {
 	}
 }
 
-function getLocation(force) {
-	hideLinks()
-	sessionStorage.removeItem('*results')
-	document.getElementById('loc').style.display = 'none'
-	navigator.geolocation.getCurrentPosition(function(p) {
-		var locLat = p.coords.latitude
-		var locLon = p.coords.longitude
-		var geocoder = new google.maps.Geocoder
-		geocoder.geocode({'location': {lat: locLat, lng: locLon}}, function(results, status) {
-			if (status === 'OK') {
-				var address = results[0].address_components
-				name = address[0].short_name + ' ' + address[1].short_name
-				var src = "https://forecast.io/embed/#lat=" + locLat.toFixed(4) + "&lon=" + locLon.toFixed(4) + "&name=" + name;
-				sessionStorage.setItem(name, src);
-				iframe.src = `${src}&color=${options.color}&font=${options.font}&units=${options.units}`;
-				geosrc()
-				showLinks()
-				if (force) {
-					sessionStorage['*pending'] = src;
-					sessionStorage['*geocode'] = true;
-					location.reload()
-				}
-				popList()
-				updatePlaceCount()
-			}
-		})
-	}, function(error) {
-			document.getElementById('top-container').style.display = ''
-			popList()
-			console.log('err =', error)
-			var err = document.getElementById('error')
-			if (error.code === 1) {
-				err.innerHTML = 'It loooks like you haven\'t enabled location permission.  Search for a location below'
-			}
-			if (error.code === 2) {
-				err.innerHTML = 'For some reason your location is unavailable - is your wifi on? modem/router working?<br>Sometimes Chrome\'s location service goes down for a little while.<br>Just search for a location below<br>\u2193'
-			}
-			if (error.code === 3) {
-				err.innerHTML = 'The location search timed out.  Search for a location below'
-			}
-			document.getElementById('saveLocbtn').style.display = 'none'	
-	})
+function updatePlaceCount() {
+	showTables.innerHTML = '\u2606: ' + getPlaces()[0];
 }
 
-function hideLinks() {
-	var links = document.getElementsByClassName('links')
-	iframe.style.display = 'none'
-	document.getElementById('framecon').style.display = ''
-	links[0].style.display = 'none'
-	links[1].style.display = 'none'
-}
-
-function showLinks() {
-	var links = document.getElementsByClassName('links')
-	iframe.style.display = ''
-	document.getElementById('framecon').style.display = 'none'
-	links[0].style.display = ''
-	links[1].style.display = ''
-}
-
-function initAutocomplete() {
-	var input = document.getElementById('locSearch');
-	autocomplete = new google.maps.places.Autocomplete(input)
-	autocomplete.addListener('place_changed', changeSrc)
-}
-
-function changeSrc() {
-	var results = autocomplete.getPlace()
-	if (results.photos) {
-		var photourls = []
-		for (var i = 0 ; i < results.photos.length ; i++) {
-			photourls.push(results.photos[i].getUrl({'maxWidth': 500, 'maxHeight': 200}))
+function getPlaces() {
+	var places = 0;
+	var recent = 0;
+	for (var key in localStorage) {
+		if (key[0] !== '*' && key !== 'length' && typeof localStorage[key] !== 'function') {
+			places++;
 		}
-		results.photourls = photourls
 	}
-	var locName = results.formatted_address
-	var frameName = results.name
-	locLat = results.geometry.location.lat().toFixed(4)
-	locLon = results.geometry.location.lng().toFixed(4)
-	var url = "https://forecast.io/embed/#lat=" + locLat + "&lon=" + locLon + "&name=" + frameName
-	sessionStorage['*pending'] = url
-	sessionStorage.setItem(locName, url)
-	sessionStorage.setItem('*' + locName, JSON.stringify(results))
-	sessionStorage.setItem('*results', JSON.stringify(results))
-	location.reload()
+	for (var key in sessionStorage) {
+		if (key[0] !== '*' && key !== 'length' && typeof sessionStorage[key] !== 'function') {
+			recent++;
+		}
+	}
+	return [places, recent];
 }
 
 // CLICK HANDLERS **********************************
@@ -300,9 +278,9 @@ showTables.onclick = function() {
 }
 
 document.getElementById('saveLocbtn').onclick = function () {
-	var name = getName();
+	var name = data.name;
 	if (name) {
-		localStorage.setItem(name, iframe.src)
+		localStorage.setItem(name, iframe.src.split('&color')[0]);
 		sessionStorage.removeItem(name)
 		if (sessionStorage['*' + name]) {
 			localStorage.setItem('*' + name, sessionStorage['*' + name])
@@ -321,55 +299,52 @@ document.getElementById('saveLocbtn').onclick = function () {
 
 document.getElementById('prevsaved').onclick = function () {
 	for (var key in localStorage) {
-		if (localStorage[key] === iframe.src) {
-			localStorage.removeItem(key)
-			var starred = '*' + key
+		if (key === data.name) {
+			localStorage.removeItem(key);
+			var starred = '*' + key;
 			if (localStorage[starred]) {
-				localStorage.removeItem(starred)
+				localStorage.removeItem(starred);
 			}
 		}
 	}
-	updatePlaceCount()
-	popList()
+	updatePlaceCount();
+	popList();
 }
 
-document.getElementById('deleteSaved').onclick = (function() {
-	var deleteSaved = false;
-	return function () {
-		if (deleteSaved) {
-			deleteSaved = false;
-			document.getElementById('deleteSaved').innerHTML = 'remove one';
-			var table = document.getElementById('savtable');
-			table.deleteRow(1);
-		}
-		else if (localStorage.length) {
-			deleteSaved = true;
-			document.getElementById('deleteSaved').innerHTML = 'CANCEL';
-			var table = document.getElementById('savtable');
-			var newRow = table.insertRow(1);
-			var newCell = newRow.insertCell(0);
-			var newDiv = document.createElement('DIV');
-			newDiv.innerHTML = '< CLICK ON A ROW TO DELETE IT >';
-			newDiv.style.textAlign = 'center';
-			newDiv.style.color = 'black';
-			newDiv.style.backgroundColor = '#f7f7f7';
-			newCell.appendChild(newDiv);
-			setTimeout(function() {
-				if (deleteSaved) {
-					document.getElementById('deleteSaved').innerHTML = 'remove one';
-					var table = document.getElementById('savtable');
-					table.deleteRow(1);
-					deleteSaved = false;
-				}
-			}, 10000);
-		}
+document.getElementById('deleteSaved').onclick = function() {
+	if (state.deleteSaved) {
+		state.deleteSaved = false;
+		document.getElementById('deleteSaved').innerHTML = 'remove one';
+		var table = document.getElementById('savtable');
+		table.deleteRow(1);
 	}
-}());
+	else if (localStorage.length) {
+		state.deleteSaved = true;
+		document.getElementById('deleteSaved').innerHTML = 'CANCEL';
+		var table = document.getElementById('savtable');
+		var newRow = table.insertRow(1);
+		var newCell = newRow.insertCell(0);
+		var newDiv = document.createElement('DIV');
+		newDiv.innerHTML = '< CLICK ON A ROW TO DELETE IT >';
+		newDiv.style.textAlign = 'center';
+		newDiv.style.color = 'black';
+		newDiv.style.backgroundColor = '#f7f7f7';
+		newCell.appendChild(newDiv);
+		setTimeout(function() {
+			if (state.deleteSaved) {
+				document.getElementById('deleteSaved').innerHTML = 'remove one';
+				var table = document.getElementById('savtable');
+				table.deleteRow(1);
+				state.deleteSaved = false;
+			}
+		}, 10000);
+	}
+}
 
 document.getElementById('remove').onclick = function () {
-	localStorage.clear()
-	updatePlaceCount()
-	popList()
+	localStorage.clear();
+	updatePlaceCount();
+	popList();
 }
 
 document.getElementById('clearrec').onclick = function () {
@@ -378,8 +353,8 @@ document.getElementById('clearrec').onclick = function () {
 			sessionStorage.removeItem(key)
 		}
 	}
-	updatePlaceCount()
-	popList()
+	updatePlaceCount();
+	popList();
 }
 
 document.getElementById('pictop').onmouseover = function() {
@@ -406,24 +381,24 @@ document.getElementById('colordefault').onclick = function() {
 document.getElementById('pictop').onclick = (function() {
 	var clicked = false;
 	var show = false;
-	var pics = document.getElementById('pics')
+	var pics = document.getElementById('pics');
 	return function() {
 		if (!clicked) {
 			clicked = true;
 			show = true;
 			if (sessionStorage['*results'] && JSON.parse(sessionStorage['*results']).photourls) {
-				var photos = JSON.parse(sessionStorage['*results']).photourls
+				var photos = JSON.parse(sessionStorage['*results']).photourls;
 				photos.forEach(function(x) {
-					var img = document.createElement('IMG')
-					img.src = x
-					img.style.margin = '5px'
-					pics.appendChild(img)
-				})
+					var img = document.createElement('IMG');
+					img.src = x;
+					img.style.margin = '5px';
+					pics.appendChild(img);
+				});
 			}
 		}
 		else {
 			show = !show;
-			pics.style.display = pics.style.display ? '' : 'none'
+			pics.style.display = pics.style.display ? '' : 'none';
 		}
 	}
 }());
