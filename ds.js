2caffbe8ce9cfb2f62dfb99cfc61c44a8911bda3
Brett Nelson <brettcnelson@gmail.com>
2019-03-15 03:24:12 -0400

    adds options

--- ds.js
+++ ds.js
@@ -1,12 +1,67 @@
 google.maps.event.addDomListener(window, 'load', initAutocomplete);
-var tables = document.getElementById('tables')
-var prevSaved = document.getElementById('prevsaved')
-var iframe = document.getElementById('forecast_embed')
-var locBtn = document.getElementById('loc')
+var tables = document.getElementById('tables');
+var prevSaved = document.getElementById('prevsaved');
+var iframe = document.getElementById('forecast_embed');
+var locBtn = document.getElementById('loc');
+var showTables = document.getElementById('show-tables');
+var defaults = {backgroundColor: '#9C9C9C',font: 'Helvetica',units: '',color: ''};
+document.getElementById('colordefault').style.backgroundColor = defaults.backgroundColor;
+var options;
+var data = {};
+chrome.storage.sync.get(defaults, setOptions);
+document.getElementById('locSearch').focus();
 
-document.getElementById('locSearch').focus()
+// ************** SET DEFAULT NOTES ****************************
+// document.getElementById('default').onclick = function() {
+// 	// console.log(iframe.src, sessionStorage);
+// 	for (var key in sessionStorage) {
+// 		if (sessionStorage[key] === iframe.src) {
+// 			var defaults = [iframe.src, sessionStorage['*' + key]];
+// 		}
+// 	}
+// 	localStorage.setItem('*default', JSON.stringify(defaults));
+// 	console.log(localStorage['*default']);
+// }
 
-var showTables = document.getElementById('show-tables')
+// if (localStorage['*default']) {
+// 	console.log('def', localStorage['*default'][0]);
+// 	iframe.src = JSON.parse(localStorage['*default'])[0];
+// }
+
+// change this to else if - if the above if statement is uncommented
+//*******************************************************************
+
+updatePlaceCount();
+if (iframe.src.split('forecast.io/embed/').length) {
+	if (sessionStorage['*results']) {
+		data.name = JSON.parse(sessionStorage['*results']).name;
+	}
+	else {
+		data.name = iframe.src.split('&name=')[1].split('&color')[0].replace(/%20/gi,' ');
+	}
+}
+if (sessionStorage['*pending']) {
+	if (sessionStorage['*geocode']) {
+		locBtn.style.display = 'none';
+		sessionStorage.removeItem('*geocode');
+	}
+	iframe.src = sessionStorage['*pending'];
+	sessionStorage.removeItem('*pending');
+	geosrc();
+	popList();
+}
+else {
+	getLocation();
+}
+
+// FUNCTIONS ***********************************************************
+
+function setOptions(opts) {
+	var {backgroundColor,...rest} = opts;
+	document.body.style.backgroundColor = backgroundColor;
+	document.body.style.fontFamily = opts.font;
+	options = rest;
+}
 
 function updatePlaceCount() {
 	showTables.innerHTML = '\u2606: ' + getPlaces()[0];
@@ -17,166 +72,122 @@ function getPlaces() {
 	var recent = 0;
 	for (var key in localStorage) {
 		if (key[0] !== '*' && key !== 'length' && typeof localStorage[key] !== 'function') {
-			places++
+			places++;
 		}
 	}
 	for (var key in sessionStorage) {
 		if (key[0] !== '*' && key !== 'length' && typeof sessionStorage[key] !== 'function') {
-			recent++
+			recent++;
 		}
 	}
-	return [places, recent]
-}
-
-// ************** SET DEFAULT NOTES ****************************
-// document.getElementById('default').onclick = function() {
-// 	// console.log(iframe.src, sessionStorage)
-// 	for (var key in sessionStorage) {
-// 		if (sessionStorage[key] === iframe.src) {
-// 			var defaults = [iframe.src, sessionStorage['*' + key]]
-// 		}
-// 	}
-// 	localStorage.setItem('*default', JSON.stringify(defaults))
-// 	console.log(localStorage['*default'])
-// }
-
-// if (localStorage['*default']) {
-// 	console.log('def', localStorage['*default'][0])
-// 	iframe.src = JSON.parse(localStorage['*default'])[0]
-// }
-
-// change this to else if - if the above if statement is uncommented
-//*******************************************************************
-
-updatePlaceCount()
-if (sessionStorage['*pending']) {
-	if (sessionStorage['*geocode']) {
-		locBtn.style.display = 'none'
-		sessionStorage.removeItem('*geocode')
-	}
-	iframe.src = sessionStorage['*pending']
-	sessionStorage.removeItem('*pending')
-	geosrc()
-	popList()
-}
-else {
-	getLocation()
+	return [places, recent];
 }
 
 function geosrc() {
-	var name = getName()
-	document.getElementById('dsanch').innerHTML = name + ' at Dark Sky'
-	var lat = iframe.src.split('=')[1].split('&')[0]
-	var lng = iframe.src.split('&lon=')[1].split('&')[0]
-	document.getElementById('dsanch').href = 'https://darksky.net/' + lat + ',' + lng
-	var mapAnch = document.getElementById('mapanch')
+	var name = getName();
+	document.getElementById('dsanch').innerHTML = name + ' at Dark Sky';
+	var lat = iframe.src.split('=')[1].split('&')[0];
+	var lng = iframe.src.split('&lon=')[1].split('&')[0];
+	document.getElementById('dsanch').href = 'https://darksky.net/' + lat + ',' + lng;
+	var mapAnch = document.getElementById('mapanch');
 	if (sessionStorage['*results']) {
-		var results = JSON.parse(sessionStorage['*results'])
-		mapAnch.innerHTML = results.name + ' on Google Maps'
-		mapAnch.href = results.url
-		var pictop = document.getElementById('pictop')
-		pictop.innerHTML = '&#9889'
+		var results = JSON.parse(sessionStorage['*results']);
+		mapAnch.innerHTML = results.name + ' on Google Maps';
+		mapAnch.href = results.url;
+		var pictop = document.getElementById('pictop');
+		pictop.innerHTML = '&#9889';
 		setTimeout(function() {
-			pictop.innerHTML = ''
-		}, 2000)
+			pictop.innerHTML = '';
+		}, 2000);
 	}
 	else {
-		var text = name + ' on Google Maps'
-		var url = 'http://maps.google.com/?q=' + lat + ',' + lng
-		mapAnch.innerHTML = text
-		mapAnch.href = url
+		var text = name + ' on Google Maps';
+		var url = 'http://maps.google.com/?q=' + lat + ',' + lng;
+		mapAnch.innerHTML = text;
+		mapAnch.href = url;
 	}
 }
 
 function getName() {
-	if (iframe.src.split('forecast.io/embed').length) {
-		var name;
-		if (sessionStorage['*results']) {
-			name = JSON.parse(sessionStorage['*results']).name
-		}
-		else {
-			name = iframe.src.split('&name=')[1].replace(/%20/gi,' ')
-		}
-	}
 	return name;
 }
 
 function popList() {
-	var currInLocal = false
+	var currInLocal = false;
 	for (var key in localStorage) {
-		if (!currInLocal && (key === iframe.src.split('&name=')[1] || (sessionStorage['*results'] && key === JSON.parse(sessionStorage['*results']).formatted_address))) {
-			currInLocal = true
+		if (!currInLocal && (key === iframe.src.split('&name=')[1].replace(/%20/gi,' ') || (sessionStorage['*results'] && key === JSON.parse(sessionStorage['*results']).formatted_address))) {
+			currInLocal = true;
 			document.getElementById('saveLocbtn').style.display = 'none'
 			document.getElementById('prevsaved').style.display = '';		
 		}
 	}
 	if (!currInLocal) {
-		document.getElementById('saveLocbtn').style.display = ''
+		document.getElementById('saveLocbtn').style.display = '';
 		document.getElementById('prevsaved').style.display = 'none';	
 	}
 
 	function pop(id1, id2, storage) {
-		var table = document.getElementById(id1)
+		var table = document.getElementById(id1);
 		while (table.rows.length) {
-			table.deleteRow(0)
+			table.deleteRow(0);
 		}
 		for (var key in storage) {
 			if (key[0] !== '*' && key !== 'length' && typeof storage[key] !== 'function') {
-				var newRow = table.insertRow(table.rows.length)
-				var newCell = newRow.insertCell(0)
-				var newText = document.createTextNode(key)
-				newCell.appendChild(newText)
-				newCell.classList.add('places')
+				var newRow = table.insertRow(table.rows.length);
+				var newCell = newRow.insertCell(0);
+				var newText = document.createTextNode(key);
+				newCell.appendChild(newText);
+				newCell.classList.add('places');
 			}
 		}
 	}
 
-	pop('recent', 'rectable', sessionStorage)
-	pop('saved', 'savtable', localStorage)
+	pop('recent', 'rectable', sessionStorage);
+	pop('saved', 'savtable', localStorage);
 
-	var places = document.querySelectorAll('.places')
+	var places = document.querySelectorAll('.places');
 	for (var i = 0 ; i < places.length ; i++) {
-		var el = places[i]
-		var search = document.getElementById('locSearch')
+		var el = places[i];
+		var search = document.getElementById('locSearch');
 
 		el.onclick = function() {
 			if (deleteSaved) {
-				deleteSaved = false
-				var table = document.getElementById('savtable')
-				table.deleteRow(1)
-				document.getElementById('deleteSaved').innerHTML = 'remove one'
+				deleteSaved = false;
+				var table = document.getElementById('savtable');
+				table.deleteRow(1);
+				document.getElementById('deleteSaved').innerHTML = 'remove one';
 				if (localStorage[this.innerHTML]) {
-					localStorage.removeItem(this.innerHTML)
-					localStorage.removeItem('*' + this.innerHTML)
-					updatePlaceCount()
-					popList()
+					localStorage.removeItem(this.innerHTML);
+					localStorage.removeItem('*' + this.innerHTML);
+					updatePlaceCount();
+					popList();
 				}
 			}
 			else {
 				var newsrc;
 				if (localStorage[this.innerHTML]) {
-					newsrc = localStorage[this.innerHTML]
+					newsrc = localStorage[this.innerHTML];
 					if (localStorage['*' + this.innerHTML]) {	
-						sessionStorage['*results'] = localStorage['*' + this.innerHTML]
+						sessionStorage['*results'] = localStorage['*' + this.innerHTML];
 					}
 					else {
-						sessionStorage.removeItem('*results')
+						sessionStorage.removeItem('*results');
 					}
 				}
 				if (sessionStorage[this.innerHTML]) {
-					newsrc = sessionStorage[this.innerHTML]
+					newsrc = sessionStorage[this.innerHTML];
 					if (sessionStorage['*' + this.innerHTML]) {
-						sessionStorage['*results'] = sessionStorage['*' + this.innerHTML]
+						sessionStorage['*results'] = sessionStorage['*' + this.innerHTML];
 					}
 					else {
-						sessionStorage.removeItem('*results')
+						sessionStorage.removeItem('*results');
 					}
 				}
 				if (!newsrc) {
-					newsrc = iframe.src
+					newsrc = iframe.src;
 				}
-				sessionStorage['*pending'] = newsrc
-				location.reload()
+				sessionStorage['*pending'] = newsrc;
+				location.reload();
 			}
 		}
 	}
@@ -193,14 +204,15 @@ function getLocation(force) {
 		geocoder.geocode({'location': {lat: locLat, lng: locLon}}, function(results, status) {
 			if (status === 'OK') {
 				var address = results[0].address_components
-				var locName = address[0].short_name + ' ' + address[1].short_name
-				iframe.src = "https://forecast.io/embed/#lat=" + locLat.toFixed(4) + "&lon=" + locLon.toFixed(4) + "&name=" + locName
-				sessionStorage.setItem(locName, iframe.src)
+				name = address[0].short_name + ' ' + address[1].short_name
+				var src = "https://forecast.io/embed/#lat=" + locLat.toFixed(4) + "&lon=" + locLon.toFixed(4) + "&name=" + name;
+				sessionStorage.setItem(name, src);
+				iframe.src = `${src}&color=${options.color}&font=${options.font}&units=${options.units}`;
 				geosrc()
 				showLinks()
 				if (force) {
-					sessionStorage['*pending'] = iframe.src
-					sessionStorage['*geocode'] = true
+					sessionStorage['*pending'] = src;
+					sessionStorage['*geocode'] = true;
 					location.reload()
 				}
 				popList()
@@ -321,39 +333,38 @@ document.getElementById('prevsaved').onclick = function () {
 	popList()
 }
 
-
-var deleteSaved = false
-document.getElementById('deleteSaved').onclick = function() {
-	if (deleteSaved) {
-		deleteSaved = false;
-		document.getElementById('deleteSaved').innerHTML = 'remove one'
-		var table = document.getElementById('savtable')
-		table.deleteRow(1)
-	}
-	else {
-		if (localStorage.length) {
+document.getElementById('deleteSaved').onclick = (function() {
+	var deleteSaved = false;
+	return function () {
+		if (deleteSaved) {
+			deleteSaved = false;
+			document.getElementById('deleteSaved').innerHTML = 'remove one';
+			var table = document.getElementById('savtable');
+			table.deleteRow(1);
+		}
+		else if (localStorage.length) {
 			deleteSaved = true;
-			document.getElementById('deleteSaved').innerHTML = 'CANCEL'	
-			var table = document.getElementById('savtable')
-			var newRow = table.insertRow(1)
-			var newCell = newRow.insertCell(0)
-			var newDiv = document.createElement('DIV')
-			newDiv.innerHTML = '< CLICK ON A ROW TO DELETE IT >'
-			newDiv.style.textAlign = 'center'
-			newDiv.style.color = 'black'
-			newDiv.style.backgroundColor = '#f7f7f7'
-			newCell.appendChild(newDiv)
+			document.getElementById('deleteSaved').innerHTML = 'CANCEL';
+			var table = document.getElementById('savtable');
+			var newRow = table.insertRow(1);
+			var newCell = newRow.insertCell(0);
+			var newDiv = document.createElement('DIV');
+			newDiv.innerHTML = '< CLICK ON A ROW TO DELETE IT >';
+			newDiv.style.textAlign = 'center';
+			newDiv.style.color = 'black';
+			newDiv.style.backgroundColor = '#f7f7f7';
+			newCell.appendChild(newDiv);
 			setTimeout(function() {
 				if (deleteSaved) {
-					document.getElementById('deleteSaved').innerHTML = 'remove one'
-					var table = document.getElementById('savtable')
-					table.deleteRow(1)
-					deleteSaved = false
+					document.getElementById('deleteSaved').innerHTML = 'remove one';
+					var table = document.getElementById('savtable');
+					table.deleteRow(1);
+					deleteSaved = false;
 				}
-			}, 10000)
+			}, 10000);
 		}
 	}
-}
+}());
 
 document.getElementById('remove').onclick = function () {
 	localStorage.clear()
@@ -375,6 +386,20 @@ document.getElementById('pictop').onmouseover = function() {
 	if (sessionStorage['*results'] && JSON.parse(sessionStorage['*results']).photourls) {
 		this.style.cursor = 's-resize'
 	}
+}
+
+document.getElementById('defaults').onclick = function() {
+	chrome.storage.sync.set(defaults,()=>{
+		chrome.storage.sync.get(defaults,setOptions);
+	});
+}
+
+document.getElementById('jscol').onchange = function(e) {
+	chrome.storage.sync.set({backgroundColor:`#${e.target.value}`},()=>chrome.storage.sync.get(defaults,setOptions));
+}
+
+document.getElementById('colordefault').onclick = function() {
+	chrome.storage.sync.set({backgroundColor:defaults.backgroundColor},()=>chrome.storage.sync.get(defaults,setOptions));
 }
 
 // ********** PICTURE / IMG FILTER NOTES ******************************
